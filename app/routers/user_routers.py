2b02578a226d74f7c84c7abcd8bb2e453335d0aa
Ramil2003 <theluckyfeed1@gmail.com>
2022-08-13 13:12:22 +0300

    Add user security. Refactoring code

---
+++ app/routers/user_routers.py
@@ -0,0 +1,47 @@
+import os
+from datetime import timedelta
+
+from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from ..depends import get_db
+from ..schemas.user_schemes import RequestUser, ResponseUser
+from ..schemas.token_schemes import Token, TokenData
+# from ..db.crud.user_cruds import
+from ..db.database import Base, engine
+from ..security import authenticate_user, create_access_token, get_current_active_user
+
+Base.metadata.create_all(engine)
+
+router = APIRouter(
+    prefix="/user",
+    tags=['User']
+)
+
+
+@router.post("/token", response_model=Token)
+async def login_for_access_token(db: Session, form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/users/me/", response_model=ResponseUser)
+async def read_users_me(current_user: ResponseUser = Depends(get_current_active_user)):
+    return current_user
+
+
+@router.get("/users/me/items/")
+async def read_own_items(current_user: ResponseUser = Depends(get_current_active_user)):
+    return [{"item_id": "Foo", "owner": current_user.username}]
